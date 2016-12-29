b142f3d8ae271801c1aa7254f12b5cc2ed30e4f1
WebDesignerKits <team@webdesignerkits.com>
2016-12-29 11:59:04 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+post = {
+  content:`
+<h1>How to connect</h1>
+<h2>Thoughts etc..</h2>
+<div id="info"></div>
+<p>
+The tale went something like this: A Jewish couple was forced to
+pull its child out of school so the family could "flee" town over
+fears stemming from "fake news" stories blaming them for the
+cancellation of a Christmas play.
+</p>      
+  `,
+  author:{
+    img:"https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-1/p320x320/13124805_10102548805744829_5469135645423581532_n.jpg?oh=b25874401fd595540d5f88040cb28b28&oe=5924870E",
+    name:"Marshall Bean",
+    link:"https://facebook.com/cabobean"
+  },
+  published : "Dec 29, 2006"
+}
